70b6c6a46219e587df0645d43f7c32c7ea392249
ItzelHernandez130613 <luzitzelhernandez.07@gmail.com>
2023-06-22 18:18:29 -0500

    Actualización de archivo EditarVacante
    Se actualizo el archivo EditarVacante

--- app/Http/Livewire/EditarVacante.php
+++ app/Http/Livewire/EditarVacante.php
@@ -72,7 +72,7 @@ class EditarVacante extends Component
         $vacante->save();
 
         // Redireccionar
-        session()->flash('mensaje', 'La vacante se actualizó correctamente');
+        session()->flash('mensaje', 'La vacante se actualizó correctamente, vuelve a actualizar');
         return redirect()->route('vacantes.index');
     }
 
